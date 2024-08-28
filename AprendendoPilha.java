46545d13582ae8941aa0338779f92dd29eac00e6
lucasboth7 <166667210+lucasboth7@users.noreply.github.com>
2024-08-28 14:50:20 -0300

    Add files via upload

---
+++ AprendendoPilha.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class AprendendoPilha {
+    public static void main(String [] args){
+        Stack<String> pilha = new Stack();
+        
+        System.out.println(pilha.size()); //
+        System.out.println(pilha.isEmpty()); //A lista esta vazia? True ou false
+        
+        
+        pilha.push("banana");
+        pilha.push("manga");
+        pilha.push("sapoti");
+        pilha.push("maça");
+        
+        System.out.println(pilha.size());
+        System.out.println(pilha.peek()); //ele só olha, vai retornar um elemento sem retira-lo
+        
+        System.out.println(pilha.pop()); // irá excluir o "maça", sendo do ultimo para o primeiro;
+        System.out.println(pilha.pop()); //irá excluir o "sapoti"
+        System.out.println(pilha.pop()); //irá excluir a "manga"
+        
+        System.out.println(pilha.size());
+        
+    }
+}
