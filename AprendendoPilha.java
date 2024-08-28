11e7d35b4e9a92ed98e6bf4467eac2b7eef65b9b
lucasboth7 <166667210+lucasboth7@users.noreply.github.com>
2024-08-28 14:52:00 -0300

    Update AprendendoPilha.java

--- AprendendoPilha.java
+++ AprendendoPilha.java
@@ -15,11 +15,11 @@ public class AprendendoPilha {
         System.out.println(pilha.size());
         System.out.println(pilha.peek()); //ele só olha, vai retornar um elemento sem retira-lo
         
-        System.out.println(pilha.pop()); // irá excluir o "maça", sendo do ultimo para o primeiro;
+        System.out.println(pilha.pop()); // irá excluir o "maça", sendo do ultimo para o primeiro.
         System.out.println(pilha.pop()); //irá excluir o "sapoti"
         System.out.println(pilha.pop()); //irá excluir a "manga"
         
-        System.out.println(pilha.size());
+        System.out.println(pilha.size()); //retorna quantos itens tem dentro da pilha.
         
     }
 }
